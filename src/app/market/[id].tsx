d2fdfb1fa49a81833a29dc003e70c3ce3a763305
Josue Costa <contato_josuecosta@hotmail.com>
2024-12-19 10:21:46 -0400

    feat: criação da pasta market

--- src/app/market/[id].tsx
+++ src/app/market/[id].tsx
@@ -1,42 +1,49 @@
-import {View, Text, Alert} from "react-native"
-import {router, useLocalSearchParams} from "expo-router"
-import { api } from "@/services/api"
-import { useEffect, useState } from "react"
-import { isLoading } from "expo-font"
-import { Loading } from "@/components/loading"
-export default function Market(){
+import { View, Text, Alert } from "react-native";
+import { Redirect, router, useLocalSearchParams } from "expo-router";
+import { api } from "@/services/api";
+import { useEffect, useState } from "react";
+import { Loading } from "@/components/loading";
+import { Cover } from "@/components/market/cover";
+import { Details, PropsDetails } from "@/components/market/details";
 
-    const params = useLocalSearchParams<{id: string}>()
-    const [data, setData] = useState()
-    const [isLoading, setIsLoading] = useState(true)
+type DataProps = PropsDetails & {
+  cover: string;
+};
 
-    async function fetchMarket(){
-        try {
-            const {data} = await api.get(`/markets/${params.id}`)
-            setData(data)
-            setIsLoading(false)
-            
-        } catch (error) {
-            console.log(error)
-            Alert.alert("Erro:", "Não foi possivel carregar os dados da loja", [
-                {text: "OK", onPress: () => router.back()}
-            ])
-        }
+export default function Market() {
+  const params = useLocalSearchParams<{ id: string }>();
+  const [data, setData] = useState<DataProps>();
+  const [isLoading, setIsLoading] = useState(true);
+
+  async function fetchMarket() {
+    try {
+      const { data } = await api.get(`/markets/${params.id}`);
+      setData(data);
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error);
+      Alert.alert("Erro:", "Não foi possivel carregar os dados da loja", [
+        { text: "OK", onPress: () => router.back() },
+      ]);
     }
+  }
 
-    useEffect(() => {
-        fetchMarket()
-    }, [params.id])
+  useEffect(() => {
+    fetchMarket();
+  }, [params.id]);
 
-    if(isLoading){
-        return(
-            <Loading/>
-        )
-    }
+  if (isLoading) {
+    return <Loading />;
+  }
 
-    return(
-        <View style={{flex: 1, justifyContent: "center", alignItems: "center"}}>
-            <Text></Text>
-        </View>
-    )
+  if(!data){
+    return <Redirect href="/home"/>
+  }
+
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Cover uri={data.cover} />
+      <Details data={data}/>
+    </View>
+  );
 }
