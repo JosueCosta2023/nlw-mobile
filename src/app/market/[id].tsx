7e6b229898a6760bb195f17fa854a75cc8345543
Josue Costa <contato_josuecosta@hotmail.com>
2024-12-20 07:53:32 -0400

    feat: conclusão de componente button na pagina de detalhes da loja.

--- src/app/market/[id].tsx
+++ src/app/market/[id].tsx
@@ -1,4 +1,4 @@
-import { View, Text, Alert } from "react-native";
+import { View, Text, Alert, Modal } from "react-native";
 import { Redirect, router, useLocalSearchParams } from "expo-router";
 import { api } from "@/services/api";
 import { useEffect, useState } from "react";
@@ -6,6 +6,7 @@ import { Loading } from "@/components/loading";
 import { Cover } from "@/components/market/cover";
 import { Details, PropsDetails } from "@/components/market/details";
 import { Coupon } from "@/components/market/coupon";
+import { Button } from "@/components/button";
 
 type DataProps = PropsDetails & {
   cover: string;
@@ -15,7 +16,8 @@ export default function Market() {
   const params = useLocalSearchParams<{ id: string }>();
   const [data, setData] = useState<DataProps>();
   const [isLoading, setIsLoading] = useState(true);
-  const [cupom, setCupom] = useState<string | null>(null)
+  const [cupom, setCupom] = useState<string | null>(null);
+  const [isVisibleCameraModal, setIsVisibleCameraModal] = useState(false);
 
   async function fetchMarket() {
     try {
@@ -30,6 +32,18 @@ export default function Market() {
     }
   }
 
+  function handleOpenCamera() {
+    try {
+      setIsVisibleCameraModal(true);
+    } catch (error) {
+      console.log(error);
+      Alert.alert(
+        "Error",
+        "Não foi possivel abrir a camera de seu dispositivo."
+      );
+    }
+  }
+
   useEffect(() => {
     fetchMarket();
   }, [params.id]);
@@ -38,17 +52,30 @@ export default function Market() {
     return <Loading />;
   }
 
-  if(!data){
-    return <Redirect href="/home"/>
+  if (!data) {
+    return <Redirect href="/home" />;
   }
 
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Cover uri={data.cover} />
-      <Details data={data}/>
+      <Details data={data} />
 
-      {cupom && <Coupon code={cupom}/>}
-      
+      {cupom && <Coupon code={cupom} />}
+
+      <View style={{ padding: 32 }}>
+        <Button onPress={() => handleOpenCamera()}>
+          <Button.Title>Ler Qr Code</Button.Title>
+        </Button>
+
+        <Modal style={{ flex: 1 }} visible={isVisibleCameraModal}>
+          <View style={{flex: 1, justifyContent: "center"}}>
+            <Button onPress={() => setIsVisibleCameraModal(false)}>
+              <Button.Title>Voltar</Button.Title>
+            </Button>
+          </View>
+        </Modal>
+      </View>
     </View>
   );
 }
